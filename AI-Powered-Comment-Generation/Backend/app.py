22cc545aacc7980e36a0e4a0af97db50bbd65b55
Piyush Kashyap <85850944+piyushkashyap07@users.noreply.github.com>
2025-04-12 02:35:54 +0530

    Update app.py

--- AI-Powered-Comment-Generation/Backend/app.py
+++ AI-Powered-Comment-Generation/Backend/app.py
@@ -27,30 +27,6 @@ class PostText(BaseModel):
 # Define the model instance
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-# @app.post("/generate_comment")
-# async def generate_comment(post_text: PostText):
-#     try:
-
-#         time.sleep(3)
-#         response = model.generate_content(prompt + post_text.text)
-#     #     response = model.generate_content(f"""                       
-                                          
-#     #    You are viewing a LinkedIn post in your feed. Carefully analyze the post, considering its tone, sentiment, and key technical points. Based on this analysis, write a short, professional, and insightful comment that:
-
-#     #     Highlights a specific technical part of the post that stood out to you, demonstrating your engagement and understanding of the topic.
-#     #     Adds value by sharing relevant thoughts, an additional technical insight, or an experience that complements or builds on the post’s content.
-#     #     Ends with a thoughtful, open-ended question that sparks further discussion and encourages engagement from others in the tech community.
-#     #     Make sure the comment is clear, concise, and adds real value to the discussion. Keep it professional but conversational, reflecting a tech-savvy perspective. Avoid generic responses and focus on contributing meaningfully to the topic. It should not look like an AI-generated comment and should have a human touch in it.
-
-#     #     Here’s the text of the post:\n\n{post_text.text}""")
-
-
-#         time.sleep(3)
-#         generated_comment = response.text.strip()
-#         return {"comment": generated_comment}
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
-
 @app.post("/generate_comment")
 async def generate_comment(post_text: PostText):
     try:
